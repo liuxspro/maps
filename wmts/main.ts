b68f6d6ccd709e06024285c2e4c40a77d6c4d275
liuxspro <liuxspro@gmail.com>
2024-12-15 22:44:57 +0800

    Feat: 增加山东天地图历史影像WMTS服务

--- wmts/main.ts
+++ wmts/main.ts
@@ -1,6 +1,6 @@
 import { Application } from "jsr:@oak/oak/application";
 import { Router } from "jsr:@oak/oak/router";
-import { XYZMap } from "./xyzmap.ts";
+import { create_capabilities } from "./wmts.ts";
 
 const router = new Router();
 
@@ -10,23 +10,20 @@ router.get("/", (ctx) => {
 
 router.get("/sdhis/:id/:el", (ctx) => {
   const { id, el } = ctx.params;
-
   const z = parseInt(el);
-
   // 获取查询参数
   const url = new URL(ctx.request.url.toString());
   const tk = url.searchParams.get("tk") || "";
 
   const server_url = `https://service.sdmap.gov.cn/hisimage/${id}`;
   const tile_url = `${server_url}?tk=${tk}&layer=c&style=c&tilematrixset=c&Service=WMTS&Request=GetTile&TileMatrix={z}&TileCol={x}&TileRow={y}`;
-  const map = new XYZMap(id, tile_url, 0, z);
 
   ctx.response.type = "text/xml";
-  ctx.response.body = map.as_wmts();
+  ctx.response.body = create_capabilities(id, tile_url, 3, z);
 });
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen({ port: 8080 });
+app.listen({ port: 80 });
